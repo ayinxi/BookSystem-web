edef81aa646b388445055786250216da0447ee97
WengZihan-seu <979225291@qq.com>
2021-07-07 15:04:27 +0800

    vvvv

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,16 @@ const routes = [
        name:'pickBook',
        component: () => import('../components/pickBook.vue')
    },
+   {
+       path:'/index', 
+       name:'index',
+       component: () => import('../views/index.vue')
+   },
+   {
+       path:'/bookDetailedInfo',
+       name:'bookDetailedInfo',
+       component: () => import('../views/bookDetailedInfo.vue')
+   }
 ]
 
 const router = new VueRouter({
---
+++ src/views/bookDetailedInfo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Home />
+    <div>
+      <el-container style="border: 1px solid #eee; margin: 5%">
+        <el-header>全部商品分类</el-header>
+        <el-container>
+          <el-aside width="40%">
+            <img src="../assets/kuku.png" />
+          </el-aside>
+          <el-main>
+            <h2>图书名称</h2>
+            <p>作者</p>
+            <p>
+              这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介这是一段简介
+            </p>
+            <p>图书分类1分类2</p>
+            <p>价格</p>
+            <el-row :gutter="20" type="flex" padding="10px 0">
+              <el-col :span="6"
+                ><el-input-number v-model="num" :min="1"></el-input-number
+              ></el-col>
+              <el-col :span="4"
+                ><el-button type="primary">加入购物车</el-button></el-col
+              >
+              <el-col :span="4"
+                ><el-button type="danger">立即购买</el-button></el-col
+              >
+            </el-row>
+          </el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </div>
+</template>
+<script>
+import Home from "../views/Home.vue";
+export default {
+  components: {
+    Home,
+  },
+  data() {
+    return {
+      num: 1,
+    };
+  },
+  methods: {},
+};
+</script>
+<style acoped>
+img {
+  width: 100%;
+  height: 95%;
+}
+</style>
