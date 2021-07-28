2a5986d42418d241b5c20749053d89f3abcc393d
ayinxi <y384879210@qq.com>
2021-07-28 18:19:53 +0800

    aaa

--- src/components/admin/manageUser.vue
+++ src/components/admin/manageUser.vue
@@ -79,9 +79,10 @@
             </el-table-column>
             <el-table-column label="操作">
               <template slot-scope="scope">
-                <el-button size="mini" type="text" @click="user.user_id=scope.row.user_id;
-                user.email=scope.row.username;user.user.name=scope.row.name;
-                user.img='';user.password=scope.row.password;">编辑</el-button>
+                <el-button size="mini" type="text" @click="user.user_id=scope.row.id;
+                user.email=scope.row.username;user.name=scope.row.name;
+                user.img=scope.row.avatar_b;user.password=scope.row.password;
+                email=scope.row.username;EditVisible=true;">编辑</el-button>
                 <el-button size="mini" type="text" @click="deleteUser(scope.row.username)">注销</el-button>
               </template>
             </el-table-column>
@@ -89,6 +90,46 @@
         </el-card>
       </div>
     </div>
+     <el-dialog title="用户信息" :visible.sync="EditVisible" center width="440px">
+        <div class="my_prompt">提示：您正在编辑邮箱为{{email}}的用户的店铺信息。</div>
+              <el-form ref="user"
+              :model="user"
+              label-width="80px"
+              :rules="rules">
+                <el-form-item label="用户头像" prop="img">
+                  <el-upload 
+                  class="avatar-uploader"
+                  ref="upload"
+                  action="http://47.94.131.208:8888"
+                  :show-file-list="false"
+                  :on-change="changePhotoFile"
+                  :on-success="handleAvatarSuccess"
+                  :before-upload="beforeAvatarUpload"
+                  :auto-upload="false"
+                  :name="user.img">
+                    <img :src="user.img" class="avatar" />
+                  </el-upload>
+                  <my-cropper
+                  ref="myCropper"
+                  @getFile="getFile"
+                  @upAgain="upAgain"
+                  ></my-cropper>
+                </el-form-item>
+                <el-form-item label="用户昵称" prop="name">
+                  <el-input v-model="user.name" autocomplete="off" maxlength="10" show-word-limit></el-input>
+                </el-form-item>
+                <el-form-item label="用户密码" prop="password">
+                  <el-input v-model="user.password" autocomplete="off" show-password></el-input>
+                </el-form-item>
+                <el-form-item label="绑定邮箱" prop="email">
+                  <el-input v-model="user.email" autocomplete="off"></el-input>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="EditVisible = false;formdata=''">取 消</el-button>
+                <el-button type="primary" @click="editUser">确 定</el-button>
+              </div>
+            </el-dialog>
   </div>
 </template>
 
@@ -108,12 +149,42 @@ export default {
       //模糊搜索
       searchText: "",
       //编辑用户信息
+      formdata: new FormData(),
+      EditVisible: false,
+      email: "",
       user: {
         img: "",
         user_id: "",
         email: "",
         password: "",
         name: "",
+      },
+      rules: {
+        email: [
+          { required: true, message: "绑定邮箱不得为空", trigger: "blur" },
+          {
+            validator: (rule, value, callback) => {
+              if (!value) {
+                callback(new Error('请输入绑定邮箱'));
+              } else {
+                if (value !== '') { 
+                  var reg=/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+                  if(!reg.test(value)){
+                  callback(new Error('请输入正确的邮箱'));
+                  }
+                }
+                callback();
+              }
+            },
+            trigger: "blur",
+          },
+        ],
+        name: [
+          { required: true, message: "用户昵称不得为空", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "密码不得为空", trigger: "blur" },
+        ],
       },
       userList:[//用户列表
       /*
@@ -148,6 +219,85 @@ export default {
       this.userDataLoading = true;
       this.getAllUser();
       this.userDataLoading = false;
+    },
+    //编辑用户信息
+    editUser() {
+      this.updateUser();
+      this.EditVisible = false;
+      this.reloadUserData();
+    },
+    //上传图片触发
+    handleCrop(file) {
+      this.$nextTick(() => {
+        this.$refs.myCropper.open(file.raw || file);
+      });
+    },
+    //点击弹框重新时触发
+    upAgain() {
+      this.$refs["upload"].$refs["upload-inner"].handleClick();
+    },
+    getFile(file) {
+      this.formdata.append("img", file);
+      //获取上传图片的本地URL，用于上传前的本地预览
+      this.user.img=window.URL.createObjectURL(file);
+      this.$refs.myCropper.close();
+    },
+    //头像上传成功之后的方法,进行回调
+    handleAvatarSuccess(res) {
+      if (res.code === 0) {
+        this.user.img = res.img;
+        // this.handleCrop(file);
+      } else {
+        this.$message.error("上传出错");
+      }
+    },
+    //上传图片时会被调用
+    changePhotoFile(file) {
+      this.handleCrop(file);
+    },
+    //头像上传之前的方法
+    beforeAvatarUpload(file) {
+      const isJPG =
+        file.type === "image/jpeg" || "image/jpg" || "image/gif" || "image/png";
+      const isLt6M = file.size / 1024 / 1024 < 6;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG、JPEG、GIF或PNG 格式!");
+      }
+      if (!isLt6M) {
+        this.$message.error("上传头像图片大小不能超过 6MB!");
+      }
+      return isJPG && isLt6M;
+    },
+    //编辑除头像以外的信息
+    updateUser() {
+      axios({
+        url: this.$store.state.yuming+"/admin/updateUser",
+        method: "POST",
+        params: {
+          user_id: this.user.user_id,
+          email: this.user.email,
+          password: this.user.password,
+          name: this.user.name,
+        },
+      })
+        .then((res) => {
+          const { code } = res.data;
+          if (code == "200") {
+            this.$message({
+              type: "success",
+              message: "编辑用户信息成功",
+        });
+          } else {
+            this.$message.error("编辑用户信息失败");
+          }
+        })
+        .catch(() => {
+          Message({
+            type: "error",
+            message: "出现错误，请稍后再试",
+          });
+        });
     },
     //获取所有用户
     getAllUser() {
@@ -289,4 +439,34 @@ export default {
   width: 200px;
   height: 200px;
 }
+
+  .my_prompt{
+    color: grey;
+    margin-bottom: 20px;
+  }
+
+   .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    margin-right: 500px;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
